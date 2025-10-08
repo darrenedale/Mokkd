c4427f3b10a084d5f193135cfb854f90fcfc4c60
Darren Edale <darren@equituk.net>
2025-10-08 19:05:13 +0100

    Copyright in source root.

--- src/Mokkd.php
+++ src/Mokkd.php
@@ -1,5 +1,22 @@
 <?php
 
+/*
+ * Copyright 2025 Darren Edale
+ *
+ * This file is part of the Mokkd package.
+ *
+ * Mokkd is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache License v2.0.
+ *
+ * Mokkd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Apache License for more details.
+ *
+ * You should have received a copy of the Apache License v2.0
+ * along with Mokkd. If not, see <http://www.apache.org/licenses/>.
+ */
+
 declare(strict_types=1);
 
 use Mokkd\Contracts\ExpectationBuilder;
